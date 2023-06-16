94ae16fd85b950bd65ed21ec29439adbabed8760
jagurto2021 <jagurto2014@gmail.com>
2023-06-16 10:10:04 -0500

    pasarela

--- assets/Keys.js
+++ assets/Keys.js
@@ -1,4 +1,4 @@
 export default {
-  public: "Aquí debes colocar tu clave publica",
-  secret: "Aquí debes colocar tu clave privada"
+  public: "pk_test_51NIMdQISVJQQrOv3e3zlZwVte0T4CdSg8luCRzkbo0PVzU28Y0HvXNJbcBRejwNdteX1gMaYW1MndxQ3C8jKlSTb00DcTAbgA6",
+  secret: "sk_test_51NIMdQISVJQQrOv3EsOWJnwvlYWt2oaZG6OePSoEpuVJNYN71cRD3LOXIElOuaz24YdVNCy1VqmNXFQS6Uy8eKIk00vY2jAwkf"
 }
--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
 import KEYS from "../assets/Keys.js"
 
 const $d = document;
-const $arepas = $d.getElementById("arepas");
-const $template = $d.getElementById("arepa-template").content;
+const $paquetes = $d.getElementById("paquetes");
+const $template = $d.getElementById("paquete-template").content;
 const $fragment = $d.createDocumentFragment();
+//headers a pasar a nuestra peticion
+//pasaremos nuestra KEY.secret, importando clave secreta en options
 const options = { headers: {Authorization: `Bearer ${KEYS.secret}`}}
-const FormatoDeMoneda = num => `$${num.slice(0, -2)}.${num.slice(-2)}`;
 
+//formato a moneda con decimales en nuevos soles
+const FormatoDeMoneda = num => `S/.${num.slice(0, -2)}.${num.slice(-2)}`;
+
+//
 let products, prices;
 
+//promesas, peticiones a una API. objeto promise
+//fetch, se pasa products y prices de stripe(documentacion)
+//si no se pone la respuesta, saldra error 401(sin autorizacion)
+// se pone como respuesta la const options, contiene la keys secret
 Promise.all([
     fetch("https://api.stripe.com/v1/products", options),
     fetch("https://api.stripe.com/v1/prices", options)
@@ -17,29 +26,35 @@ Promise.all([
 .then(json => {
     products = json[0].data;
     prices = json[1].data;
+
     prices.forEach(el => {
         let productData = products.filter(product => product.id === el.product);
         
-        $template.querySelector(".arepa").setAttribute("data-price", el.id);
+        $template.querySelector(".paquete").setAttribute("data-price", el.id);
         $template.querySelector("img").src = productData[0].images[0];
         $template.querySelector("img").alt = productData[0].name;
+        //arma precio, muestra
         $template.querySelector("figcaption").innerHTML = `${productData[0].name} ${FormatoDeMoneda(el.unit_amount_decimal)} ${(el.currency).toUpperCase()}`;
 
         let $clone = $d.importNode($template, true);
 
         $fragment.appendChild($clone);
     });
-
-    $arepas.appendChild($fragment);
-})
+//
+    $paquetes.appendChild($fragment);
+}) 
+//en caso de error, metemos un catch
 .catch(error => {
     let message = error.statuText || "Ocurrió un error en la petición";
-
-    $arepas.innerHTML = `Error: ${error.status}: ${message}`;
+    //para que nos traiga el codigo del erro con el mensaje
+    $paquetes.innerHTML = `Error: ${error.status}: ${message}`;
 })
 
+
 $d.addEventListener("click", e => {
-    if (e.target.matches(".arepas *")) {
+    //viene de section
+    if (e.target.matches(".paquetes *")) {
+        //alert("sss")
         let priceId = e.target.parentElement.getAttribute("data-price");
 
         Stripe(KEYS.public).redirectToCheckout({
@@ -47,13 +62,13 @@ $d.addEventListener("click", e => {
                 price: priceId,
                 quantity: 1
             }],
-            mode: "subscription",
+            mode: "payment",
             successUrl:"http://127.0.01:5500/assets/success.html",
-            cancelUrl:"http://127.0.01:5500/assets/cancel.html"
+            cancelUrl:"http://127.0.01:5500/assets/success.html"
         })
         .then(res => {
             if (res.error){
-                $arepas.insertAdjacentElement("afterend", res.error.message)
+                $paquetes.insertAdjacentElement("afterend", res.error.message)
             }
         })
     }
